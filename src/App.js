163a30c4838961ba6e1c663d991008106ad7614e
Daniel Woelfel <dwwoelfel@gmail.com>
2019-01-22 12:44:00 -0800

    Example usage of OneGraph's GraphiQL Explorer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// @flow
+
+import React, {Component} from 'react';
+import GraphiQL from 'graphiql';
+import GraphiQLExplorer from 'graphiql-explorer';
+import {getIntrospectionQuery, buildClientSchema} from 'graphql';
+
+import 'graphiql/graphiql.css';
 import './App.css';
 
-class App extends Component {
+import type {GraphQLSchema} from 'graphql';
+
+function fetcher(params: Object): Object {
+  return fetch(
+    'https://serve.onegraph.com/dynamic?app_id=c333eb5b-04b2-4709-9246-31e18db397e1',
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    },
+  )
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(responseBody) {
+      try {
+        return JSON.parse(responseBody);
+      } catch (e) {
+        return responseBody;
+      }
+    });
+}
+
+const DEFAULT_QUERY = `{
+  npm {
+    package(name: "graphql") {
+      name
+      downloads {
+        lastMonth {
+          count
+        }
+      }
+    }
+  }
+}`;
+
+type State = {
+  schema: ?GraphQLSchema,
+  query: string,
+  explorerIsOpen: boolean,
+};
+
+class App extends Component<{}, State> {
+  _graphiql: GraphiQL;
+  state = {schema: null, query: DEFAULT_QUERY, explorerIsOpen: true};
+
+  componentDidMount() {
+    fetcher({
+      query: getIntrospectionQuery(),
+    }).then(result => {
+      this.setState({schema: buildClientSchema(result.data)});
+    });
+  }
+
+  _handleEditQuery = (query: string): void => this.setState({query});
+
+  _handleToggleExplorer = () => {
+    this.setState({explorerIsOpen: !this.state.explorerIsOpen});
+  };
+
   render() {
+    const {query, schema} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="graphiql-container">
+        <GraphiQLExplorer
+          schema={schema}
+          query={query}
+          onEdit={this._handleEditQuery}
+          explorerIsOpen={this.state.explorerIsOpen}
+          onToggleExplorer={this._handleToggleExplorer}
+        />
+        <GraphiQL
+          ref={ref => (this._graphiql = ref)}
+          fetcher={fetcher}
+          schema={schema}
+          query={query}
+          onEditQuery={this._handleEditQuery}>
+          <GraphiQL.Toolbar>
+            <GraphiQL.Button
+              onClick={() => this._graphiql.handlePrettifyQuery()}
+              label="Prettify"
+              title="Prettify Query (Shift-Ctrl-P)"
+            />
+            <GraphiQL.Button
+              onClick={() => this._graphiql.handleToggleHistory()}
+              label="History"
+              title="Show History"
+            />
+            <GraphiQL.Button
+              onClick={this._handleToggleExplorer}
+              label="Explorer"
+              title="Toggle Explorer"
+            />
+          </GraphiQL.Toolbar>
+        </GraphiQL>
       </div>
     );
   }
