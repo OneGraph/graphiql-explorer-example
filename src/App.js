7aae227268f0c2d8c8606345ed874c5fa58fbbb4
Gareth Davies <hello@garethtdavies.com>
2021-01-31 14:51:52 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import type { GraphQLSchema } from "graphql";
 
 function fetcher(params: Object): Object {
   return fetch(
-    "https://serve.onegraph.com/dynamic?app_id=c333eb5b-04b2-4709-9246-31e18db397e1",
+    "https://graphql.minaexplorer.com",
     {
       method: "POST",
       headers: {
@@ -36,46 +36,7 @@ function fetcher(params: Object): Object {
     });
 }
 
-const DEFAULT_QUERY = `# shift-option/alt-click on a query below to jump to it in the explorer
-# option/alt-click on a field in the explorer to select all subfields
-query npmPackage {
-  npm {
-    package(name: "onegraph-apollo-client") {
-      name
-      homepage
-      downloads {
-        lastMonth {
-          count
-        }
-      }
-    }
-  }
-}
-
-query graphQLPackage {
-  npm {
-    package(name: "graphql") {
-      name
-      homepage
-      downloads {
-        lastMonth {
-          count
-        }
-      }
-    }
-  }
-}
-
-fragment bundlephobiaInfo on BundlephobiaDependencyInfo {
-  name
-  size
-  version
-  history {
-    dependencyCount
-    size
-    gzip
-  }
-}`;
+const DEFAULT_QUERY = ``;
 
 type State = {
   schema: ?GraphQLSchema,
